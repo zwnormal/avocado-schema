21f47b84ad930e8db5004fccb34de3e44bae8ade
Wei Zheng <zwnormal@gmail.com>
2023-10-08 08:20:43 +1100

    Remove unused code

--- src/core/field/mod.rs
+++ src/core/field/mod.rs
@@ -5,8 +5,7 @@ use crate::core::field::float::FloatField;
 use crate::core::field::integer::IntegerField;
 use crate::core::field::object::ObjectField;
 use crate::core::field::string::StringField;
-use serde::de::{Error, Visitor};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Serialize, Serializer};
 use std::fmt;
 use std::fmt::{Debug, Formatter};
 
@@ -18,49 +17,6 @@ pub enum FieldType {
     Boolean,
     Object,
     Array,
-}
-
-impl Serialize for FieldType {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(format!("{}", self).as_str())
-    }
-}
-
-impl<'de> Deserialize<'de> for FieldType {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(FieldTypeVisitor)
-    }
-}
-
-struct FieldTypeVisitor;
-
-impl<'de> Visitor<'de> for FieldTypeVisitor {
-    type Value = FieldType;
-
-    fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-        write!(formatter, "field type needs to be a valid type of string")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        match v {
-            "string" => Ok(FieldType::String),
-            "integer" => Ok(FieldType::Integer),
-            "float" => Ok(FieldType::Float),
-            "boolean" => Ok(FieldType::Boolean),
-            "array" => Ok(FieldType::Array),
-            "object" => Ok(FieldType::Object),
-            _ => Err(Error::custom("invalid field type")),
-        }
-    }
 }
 
 impl fmt::Display for FieldType {
