3e2ebfa28416f1152deb3ea30eb533d44feb5147
Wei Zheng <zwnormal@gmail.com>
2023-11-02 08:54:39 +1100

    Refactor the derive module

---
+++ schema_derive/src/reflect.rs
@@ -0,0 +1,42 @@
+pub(crate) mod reflect {
+    use proc_macro::TokenStream;
+    use quote::quote;
+    use syn;
+    use syn::punctuated::Punctuated;
+    use syn::token::Comma;
+    use syn::{Data, Field, Fields};
+
+    pub(crate) fn impl_reflect_macro(ast: syn::DeriveInput) -> TokenStream {
+        let struct_ident = ast.ident;
+        let struct_fields = fields(ast.data);
+
+        let field_values = struct_fields.iter().map(|field| {
+            let field_name = &field.ident;
+            quote!((stringify!(#field_name).to_string(), self.#field_name.field_value()))
+        });
+
+        let gen = quote! {
+            impl Reflect for #struct_ident {
+                fn field_value(&self) -> FieldValue {
+                    FieldValue::Object(::std::collections::BTreeMap::from([
+                        #(#field_values),*
+                    ]))
+                }
+            }
+        };
+        gen.into()
+    }
+
+    fn fields(data: Data) -> Punctuated<Field, Comma> {
+        match data {
+            Data::Struct(s) => {
+                if let Fields::Named(named_fields) = s.fields {
+                    named_fields.named
+                } else {
+                    unimplemented!("derive(Reflect) only supports named fields")
+                }
+            }
+            _ => unimplemented!("derive(Reflect) only supports Struct"),
+        }
+    }
+}
