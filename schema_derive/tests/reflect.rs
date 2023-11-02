308d7ac2f821b1743161a8a52457c8a5e81deeed
Wei Zheng <Wei.Zheng@iress.com.au>
2023-11-02 13:49:39 +1100

    Add assert eq

--- schema_derive/tests/reflect.rs
+++ schema_derive/tests/reflect.rs
@@ -1,10 +1,30 @@
+use avocado_schema::core::value::{FieldValue, Reflect};
 use avocado_schema_derive::Reflect;
+use std::collections::BTreeMap;
 
 #[derive(Reflect)]
 struct Client {
     first_name: String,
-    second_name: String,
+    last_name: String,
 }
 
 #[test]
-fn main() {}
+fn main() {
+    let client = Client {
+        first_name: "Robert".to_string(),
+        last_name: "Li".to_string(),
+    };
+    assert_eq!(
+        client.field_value(),
+        FieldValue::Object(BTreeMap::from([
+            (
+                "first_name".to_string(),
+                FieldValue::String("Robert".to_string())
+            ),
+            (
+                "last_name".to_string(),
+                FieldValue::String("Li".to_string())
+            )
+        ]))
+    )
+}
